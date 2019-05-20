51b696be2988cbf4bac0e896c05b754dd5d54a35
Pashahasband <pashajurev@mail.ru>
2019-05-20 06:47:23 +0300

    Создал отдельный проект под нужды задания

---
+++ RegeditService/RegeditService/Service1.cs
@@ -0,0 +1,30 @@
+﻿using Regedit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegeditService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            RegeditHelper.SetRegistryKeyValue("URL", "localhost");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
